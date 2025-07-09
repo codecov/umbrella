492864502fbdbc7365b97f0e060d350ca88a7084
Suejung Shin <suejung.shin@sentry.io>
2025-07-08 17:20:02 -0700

    fix: Add caps to celery work

--- apps/worker/services/cleanup/tests/test_pulls.py
+++ apps/worker/services/cleanup/tests/test_pulls.py
@@ -1,12 +1,8 @@
 import pytest
-
 from services.cleanup.pulls import cleanup_flare
 from services.cleanup.utils import CleanupContext
 from shared.django_apps.core.models import Pull, PullStates
-from shared.django_apps.core.tests.factories import (
-    PullFactory,
-    RepositoryFactory,
-)
+from shared.django_apps.core.tests.factories import PullFactory, RepositoryFactory
 from shared.storage.exceptions import FileNotInStorageError
 
 
@@ -78,7 +74,7 @@ def test_flare_cleanup_in_regular_cleanup(
 
     # Run flare cleanup
     context = CleanupContext()
-    cleanup_flare(context=context)
+    cleanup_flare(context=context, start_id=1, max_id=1000)
 
     # Check that the non-open pulls have had their flare data cleaned
     # Note: there is a cache for flare on the object (all ArchiveFields have this),
@@ -112,7 +108,7 @@ def test_flare_cleanup_in_regular_cleanup(
     # Verify that running cleanup again doesn't process the already cleaned pulls
     mock_delete.reset_mock()
     context = CleanupContext()
-    cleanup_flare(context=context)
+    cleanup_flare(context=context, start_id=1, max_id=1000)
 
     # Verify no delete calls were made for already processed pulls
     mock_delete.assert_not_called()
@@ -146,7 +142,7 @@ def test_flare_cleanup_failed_deletion(transactional_db, mocker, mock_archive_st
 
     # Run cleanup
     context = CleanupContext()
-    cleanup_flare(context=context)
+    cleanup_flare(context=context, start_id=1, max_id=1000)
 
     # Verify the delete_file was called
     storage_path = failed_deletion_pull._flare_storage_path
@@ -188,7 +184,7 @@ def test_flare_cleanup_file_not_in_storage(
 
     # Run cleanup
     context = CleanupContext()
-    cleanup_flare(context=context)
+    cleanup_flare(context=context, start_id=1, max_id=1000)
 
     # Verify the delete_file was called
     storage_path = file_not_in_storage_pull._flare_storage_path
@@ -234,7 +230,7 @@ def test_flare_cleanup_general_exception(
 
     # Run cleanup
     context = CleanupContext()
-    cleanup_flare(context=context)
+    cleanup_flare(context=context, start_id=1, max_id=1000)
 
     # Verify the delete_file was called
     storage_path = exception_pull._flare_storage_path
