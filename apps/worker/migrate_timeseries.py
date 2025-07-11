81e82667a5543ccf56e22bc3dc2f66c77ed27f35
Elio Di Nino <elio.dinino@sentry.io>
2025-07-11 15:12:19 -0400

    fix(worker): Run timeseries migrations on "timeseries" database
    This is in addition to running them on “ta_timeseries”.

--- apps/worker/migrate_timeseries.py
+++ apps/worker/migrate_timeseries.py
@@ -7,8 +7,17 @@ from django.db import connections
 
 from shared.django_apps.utils.config import get_settings_module
 
+#################################################################
+# Keep in sync between apps/codecov-api/migrate_timeseries.py and
+# apps/worker/migrate_timeseries.py
+
+# Note: this is the only difference from the worker version
+parent_module = "django_scaffold"
+#################################################################
+
+
 # Setup Django environment
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", get_settings_module("django_scaffold"))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", get_settings_module(parent_module))
 django.setup()
 
 from django.conf import settings  # noqa: E402
@@ -17,29 +26,35 @@ logger = logging.getLogger(__name__)
 
 
 def run_migrate_commands():
-    try:
-        if settings.TA_TIMESERIES_ENABLED:
-            logger.info("Running ta_timeseries migrations")
-            with connections["ta_timeseries"].cursor() as cursor:
-                cursor.execute(
-                    "SELECT _timescaledb_internal.stop_background_workers();"
+    for app, setting in [
+        ("timeseries", settings.TIMESERIES_ENABLED),
+        ("ta_timeseries", settings.TA_TIMESERIES_ENABLED),
+    ]:
+        try:
+            if setting and (
+                f"shared.django_apps.{app}" in settings.INSTALLED_APPS
+                or app in settings.INSTALLED_APPS
+            ):
+                logger.info(f"Running {app} migrations")
+                with connections[app].cursor() as cursor:
+                    cursor.execute(
+                        "SELECT _timescaledb_internal.stop_background_workers();"
+                    )
+                call_command(
+                    "migrate",
+                    database=app,
+                    app_label=app,
+                    settings=os.environ["DJANGO_SETTINGS_MODULE"],
+                    verbosity=1,
                 )
-            call_command(
-                "migrate",
-                database="ta_timeseries",
-                app_label="ta_timeseries",
-                settings=get_settings_module("django_scaffold"),
-                verbosity=1,
-            )
-            with connections["ta_timeseries"].cursor() as cursor:
-                cursor.execute(
-                    "SELECT _timescaledb_internal.start_background_workers();"
-                )
-        else:
-            logger.info("Skipping ta_timeseries migrations")
-
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
+                with connections[app].cursor() as cursor:
+                    cursor.execute(
+                        "SELECT _timescaledb_internal.start_background_workers();"
+                    )
+            else:
+                logger.info(f"Skipping {app} migrations")
+        except Exception as e:
+            logger.error(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
