13f21d92d6293f6dde84f954dfb2c5fd9d73bf47
joseph-sentry <136376984+joseph-sentry@users.noreply.github.com>
2025-08-18 12:17:46 -0400

    debug: trying to figure out where these interface errors are coming from (#415)

--- apps/worker/tasks/base.py
+++ apps/worker/tasks/base.py
@@ -1,10 +1,12 @@
 import logging
 from datetime import datetime
+from time import monotonic
 
 import sentry_sdk
 from celery._state import get_current_task
 from celery.exceptions import MaxRetriesExceededError, SoftTimeLimitExceeded
 from celery.worker.request import Request
+from django.db import InterfaceError, connections
 from sqlalchemy.exc import (
     DataError,
     IntegrityError,
@@ -265,6 +267,23 @@ class BaseCodecovTask(celery_app.Task):
             try:
                 with self.task_core_runtime.time():  # Timer isn't tested
                     return self.run_impl(db_session, *args, **kwargs)
+            except InterfaceError as ex:
+                conn = connections["default"]
+                log.exception(
+                    "Interface error while talking to database",
+                    extra={
+                        "task_args": args,
+                        "task_kwargs": kwargs,
+                        "close_at": conn.close_at,
+                        "closed_in_transaction": conn.closed_in_transaction,
+                        "errors_occurred": conn.errors_occurred,
+                        "monotonic_time": monotonic(),
+                    },
+                    exc_info=True,
+                )
+                sentry_sdk.capture_exception(
+                    ex,
+                )
             except (DataError, IntegrityError):
                 log.exception(
                     "Errors related to the constraints of database happened",
