8d715f3b7d012f333caeadf25e29de4a17118a52
jasonford <jason.ford@sentry.io>
2025-08-26 08:42:05 -0500

    fix: correct IP extraction logic in LoginMixin and AsyncGraphqlView
    Updated the IP extraction method to use the first IP from the HTTP_X_FORWARDED_FOR header in both the LoginMixin and AsyncGraphqlView classes, improving the accuracy of client IP retrieval.

--- apps/codecov-api/graphql_api/views.py
+++ apps/codecov-api/graphql_api/views.py
@@ -424,7 +424,7 @@ class AsyncGraphqlView(GraphQLAsyncView):
     def get_client_ip(self, request: WSGIRequest) -> str:
         x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
         if x_forwarded_for:
-            ip = x_forwarded_for.split(",")[-2]
+            ip = x_forwarded_for.split(",")[0]
         else:
             ip = request.META.get("REMOTE_ADDR")
         return ip
