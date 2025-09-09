eefa7ed5cc5c737e3dc933e74ce6aac2a1f2cc00
Ajay Singh <ajay.singh@sentry.io>
2025-09-09 11:55:41 -0700

    update logic to account for existing accounts as well as unlinking

--- apps/codecov-api/api/sentry/urls.py
+++ apps/codecov-api/api/sentry/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import account_link
+from .views import account_link, account_unlink
 
 urlpatterns = [
     path("internal/account/link/", account_link, name="account-link"),
+    path("internal/account/unlink", account_unlink, name="account-unlink"),
 ]
