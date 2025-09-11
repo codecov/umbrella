3e6b5b30a0724c0555ec0d14894d6bb15d868a18
Joseph Sawaya <joseph.sawaya@sentry.io>
2025-09-11 18:50:22 -0400

    test: update testrun factory and ta_timeseries and ta_finish_upload tests

--- apps/worker/services/test_analytics/tests/test_ta_timeseries.py
+++ apps/worker/services/test_analytics/tests/test_ta_timeseries.py
@@ -14,9 +14,13 @@ from shared.django_apps.ta_timeseries.models import (
     Testrun,
     calc_test_id,
 )
+from shared.django_apps.ta_timeseries.tests.factories import (
+    TestrunFactory,
+)
+
+pytestmark = pytest.mark.django_db(databases=["ta_timeseries"])
 
 
-@pytest.mark.django_db(databases=["ta_timeseries"])
 def test_insert_testrun():
     insert_testrun(
         timestamp=datetime.now(),
@@ -44,199 +48,58 @@ def test_insert_testrun():
     )
 
     t = Testrun.objects.get(
-        name="test_name",
-        classname="test_classname",
-        testsuite="test_suite",
-        failure_message=None,
-        filename="test_filename",
+        test_id=calc_test_id("test_name", "test_classname", "test_suite")
     )
     assert t.outcome == "pass"
 
 
-@pytest.mark.django_db(databases=["ta_timeseries"])
 def test_pr_comment_agg():
-    insert_testrun(
-        timestamp=datetime.now(),
-        repo_id=1,
-        commit_sha="commit_sha",
-        branch="branch",
-        upload_id=1,
-        flags=["flag1", "flag2"],
-        parsing_info={
-            "framework": "Pytest",
-            "testruns": [
-                {
-                    "name": "test_name",
-                    "classname": "test_classname",
-                    "computed_name": "computed_name",
-                    "duration": 1.0,
-                    "outcome": "pass",
-                    "testsuite": "test_suite",
-                    "failure_message": None,
-                    "filename": "test_filename",
-                    "build_url": None,
-                }
-            ],
-        },
-    )
-
-    agg = get_pr_comment_agg(1, "commit_sha")
-    assert agg == {
-        "passed": 1,
-        "failed": 0,
-        "skipped": 0,
-    }
+    TestrunFactory.create(commit_sha="agg_commit")
+    agg = get_pr_comment_agg(1, "agg_commit")
+    assert agg == {"passed": 1, "failed": 0, "skipped": 0}
 
 
-@pytest.mark.django_db(databases=["ta_timeseries"])
 def test_pr_comment_failures():
-    insert_testrun(
-        timestamp=datetime.now(),
-        repo_id=1,
-        commit_sha="commit_sha",
-        branch="branch",
-        upload_id=1,
+    TestrunFactory.create(
+        commit_sha="failure_commit",
+        outcome="failure",
+        failure_message="test failed",
         flags=["flag1", "flag2"],
-        parsing_info={
-            "framework": "Pytest",
-            "testruns": [
-                {
-                    "name": "test_name",
-                    "classname": "test_classname",
-                    "computed_name": "computed_name",
-                    "duration": 1.0,
-                    "outcome": "failure",
-                    "testsuite": "test_suite",
-                    "failure_message": "failure_message",
-                    "filename": "test_filename",
-                    "build_url": None,
-                }
-            ],
-        },
     )
 
-    failures = get_pr_comment_failures(1, "commit_sha")
+    failures = get_pr_comment_failures(1, "failure_commit")
     assert len(failures) == 1
     failure = failures[0]
-    assert failure["test_id"] == calc_test_id(
-        "test_name", "test_classname", "test_suite"
-    )
-    assert failure["computed_name"] == "computed_name"
-    assert failure["failure_message"] == "failure_message"
-    assert failure["duration_seconds"] == 1.0
+    assert failure["failure_message"] == "test failed"
     assert failure["upload_id"] == 1
     assert failure["flags"] == ["flag1", "flag2"]
 
 
-@pytest.mark.django_db(databases=["ta_timeseries"])
 def test_get_testruns_for_flake_detection(db):
-    test_ids = {calc_test_id("flaky_test_name", "test_classname", "test_suite")}
-    insert_testrun(
-        timestamp=datetime.now(),
-        repo_id=1,
-        commit_sha="commit_sha",
-        branch="branch",
-        upload_id=1,
-        flags=["flag1", "flag2"],
-        parsing_info={
-            "framework": "Pytest",
-            "testruns": [
-                {
-                    "name": "test_name",
-                    "classname": "test_classname",
-                    "computed_name": "computed_name",
-                    "duration": 1.0,
-                    "outcome": "failure",
-                    "testsuite": "test_suite",
-                    "failure_message": "failure_message",
-                    "filename": "test_filename",
-                    "build_url": None,
-                },
-                {
-                    "name": "flaky_test_name",
-                    "classname": "test_classname",
-                    "computed_name": "computed_name",
-                    "duration": 1.0,
-                    "outcome": "failure",
-                    "testsuite": "test_suite",
-                    "failure_message": "failure_message",
-                    "filename": "test_filename",
-                    "build_url": None,
-                },
-                {
-                    "name": "flaky_test_name",
-                    "classname": "test_classname",
-                    "computed_name": "computed_name",
-                    "duration": 1.0,
-                    "outcome": "pass",
-                    "testsuite": "test_suite",
-                    "failure_message": "failure_message",
-                    "filename": "test_filename",
-                    "build_url": None,
-                },
-                {
-                    "name": "test_name",
-                    "classname": "test_classname",
-                    "computed_name": "computed_name",
-                    "duration": 1.0,
-                    "outcome": "pass",
-                    "testsuite": "test_suite",
-                    "failure_message": "failure_message",
-                    "filename": "test_filename",
-                    "build_url": None,
-                },
-            ],
-        },
-        flaky_test_ids=test_ids,
+    test_ids = {calc_test_id("flaky_test", "TestClass", "test_suite")}
+
+    TestrunFactory.create(outcome="failure")
+    TestrunFactory.create(outcome="flaky_fail")
+    TestrunFactory.create(
+        outcome="pass", test_id=calc_test_id("flaky_test", "TestClass", "test_suite")
     )
+    TestrunFactory.create(outcome="pass")
 
     testruns = get_testruns_for_flake_detection(1, test_ids)
     assert len(testruns) == 3
     assert testruns[0].outcome == "failure"
-    assert testruns[0].failure_message == "failure_message"
-    assert testruns[0].name == "test_name"
     assert testruns[1].outcome == "flaky_fail"
-    assert testruns[1].failure_message == "failure_message"
-    assert testruns[1].name == "flaky_test_name"
     assert testruns[2].outcome == "pass"
-    assert testruns[2].failure_message == "failure_message"
-    assert testruns[2].name == "flaky_test_name"
 
 
-@pytest.mark.django_db(databases=["ta_timeseries"])
 @freeze_time("2025-01-01")
 def test_update_testrun_to_flaky():
-    insert_testrun(
-        timestamp=datetime.now(),
-        repo_id=1,
-        commit_sha="commit_sha",
-        branch="branch",
-        upload_id=1,
-        flags=["flag1", "flag2"],
-        parsing_info={
-            "framework": "Pytest",
-            "testruns": [
-                {
-                    "name": "test_name",
-                    "classname": "test_classname",
-                    "computed_name": "computed_name",
-                    "duration": 1.0,
-                    "outcome": "failure",
-                    "testsuite": "test_suite",
-                    "failure_message": "failure_message",
-                    "filename": "test_filename",
-                    "build_url": None,
-                },
-            ],
-        },
+    testrun = TestrunFactory.create(
+        outcome="failure",
     )
     update_testrun_to_flaky(
-        datetime.now(),
-        calc_test_id("test_name", "test_classname", "test_suite"),
+        testrun.timestamp,
+        testrun.test_id,
     )
-    testrun = Testrun.objects.get(
-        name="test_name",
-        classname="test_classname",
-        testsuite="test_suite",
-    )
+    testrun = Testrun.objects.get(test_id=testrun.test_id)
     assert testrun.outcome == "flaky_fail"
