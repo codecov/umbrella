bcaa94cbf9e0fabcedef126badec0965acd7dc33
Max <max.weng@sentry.io>
2025-10-06 16:21:03 -0700

    cleanup

--- apps/worker/tasks/upload.py
+++ apps/worker/tasks/upload.py
@@ -564,23 +564,6 @@ class UploadTask(BaseCodecovTask, name=upload_task_name):
             report_service=report_service,
         )
 
-        # FOR TESTING: Inject a retriable error for commits ending with 'test'
-        # This will create a FILE_NOT_IN_STORAGE error that can be resent from admin panel
-        # if True:
-        #     log.info("TESTING: Injecting FILE_NOT_IN_STORAGE error for admin panel testing")
-        #     # Get upload IDs for breadcrumb
-        #     upload_ids = [int(upload["upload_id"]) for upload in upload_argument_list]
-        #     self._call_upload_breadcrumb_task(
-        #         commit_sha=commit.commitid,
-        #         repo_id=commit.repoid,
-        #         milestone=Milestones.PROCESSING_UPLOAD,
-        #         upload_ids=upload_ids,
-        #         error=Errors.FILE_NOT_IN_STORAGE,
-        #         error_text="Testing: Simulated file not found error for admin resend testing"
-        #     )
-        #     # Return early to simulate the failure
-        #     return {"was_setup": was_setup, "was_updated": was_updated, "tasks_were_scheduled": False}
-
         if upload_argument_list:
             db_session.commit()
 
