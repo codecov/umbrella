1c29647b9b6c1af7c1dc545f638cc5513e0d8322
Adrian <adrian.bonilla@sentry.io>
2025-10-23 14:37:01 -0700

    feat: create index for faster public ui fetching (#537)
    * create index for faster public ui fetching
    
    * write raw sql for clarity
    
    * add atomic = false

---
+++ libs/shared/shared/django_apps/ta_timeseries/migrations/0036_testrun_ta_ts__repo_id_idx.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    atomic = False
+
+    dependencies = [
+        ("ta_timeseries", "0035_alter_branch_test_aggregate_daily_policy"),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql=(
+                "CREATE INDEX IF NOT EXISTS ta_ts__repo_id_idx "
+                "ON ta_timeseries_testrun (repo_id) "
+                "WITH (timescaledb.transaction_per_chunk);"
+            ),
+            reverse_sql=("DROP INDEX IF EXISTS ta_ts__repo_id_idx;"),
+        ),
+    ]
