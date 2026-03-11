47922fbdd2728ebea0db1d452e6c8184293cd184
jasonford <jason.ford@sentry.io>
2026-03-11 11:34:10 -0500

    removing retry for DeleteOwnerTask

--- apps/worker/tasks/delete_owner.py
+++ apps/worker/tasks/delete_owner.py
@@ -1,7 +1,6 @@
 import logging
 
-from celery.exceptions import SoftTimeLimitExceeded
-
+# from celery.exceptions import SoftTimeLimitExceeded
 from app import celery_app
 from services.cleanup.owner import cleanup_owner
 from services.cleanup.utils import CleanupSummary
@@ -16,10 +15,10 @@ class DeleteOwnerTask(BaseCodecovTask, name=delete_owner_task_name):
     max_retries = None  # aka, no limit on retries
 
     def run_impl(self, _db_session, ownerid: int) -> CleanupSummary:
-        try:
-            return cleanup_owner(ownerid)
-        except SoftTimeLimitExceeded:
-            raise self.retry()
+        # try:
+        return cleanup_owner(ownerid)
+        # except SoftTimeLimitExceeded:
+        #   raise self.retry()
 
 
 RegisteredDeleteOwnerTask = celery_app.register_task(DeleteOwnerTask())
