3d2ed6b0c3e650552348c3ae6ba411d55dead680
jasonford <jason.ford@sentry.io>
2026-03-11 12:45:33 -0500

    updated comment

--- apps/worker/tasks/delete_owner.py
+++ apps/worker/tasks/delete_owner.py
@@ -13,10 +13,11 @@ class DeleteOwnerTask(BaseCodecovTask, name=delete_owner_task_name):
     acks_late = True  # retry the task when the worker dies for whatever reason
     max_retries = None  # aka, no limit on retries
 
+# removed retry logic because it was causing lock contention in run_impl
+
     def run_impl(self, _db_session, ownerid: int) -> CleanupSummary:
         # Purposefully not catching errors since this task runs on a cron
         return cleanup_owner(ownerid)
-#removed retry logic because it was causing lock contention
 
 RegisteredDeleteOwnerTask = celery_app.register_task(DeleteOwnerTask())
 delete_owner_task = celery_app.tasks[DeleteOwnerTask.name]
