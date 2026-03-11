e8d849d41168c2fdb8aee801c8e93e7b9b33f508
Tom Hu <thomas.hu@sentry.io>
2026-03-12 00:28:18 +0900

    Revert "refactor(worker): remove ScheduledTasksResult wrapper in upload task"
    This reverts commit 215847caf7cfd2a23c3e53050ba72afd9b617f9f.

--- apps/worker/tasks/upload.py
+++ apps/worker/tasks/upload.py
@@ -194,11 +194,17 @@ class CreateUploadResponse(TypedDict):
     upload_flag_map: dict[Upload, list | str | None]
 
 
+class ScheduledTasksResult:
+    def __init__(self, task_ids: list[str]):
+        self._task_ids = task_ids
+
+    def as_tuple(self):
+        return tuple(self._task_ids)
+
+
 def _scheduled_task_ids(scheduled_tasks) -> tuple[str, ...]:
-    if isinstance(scheduled_tasks, tuple):
-        return scheduled_tasks
-    if isinstance(scheduled_tasks, list):
-        return tuple(scheduled_tasks)
+    if isinstance(scheduled_tasks, ScheduledTasksResult):
+        return scheduled_tasks.as_tuple()
 
     task_id = getattr(scheduled_tasks, "id", None)
     if task_id:
@@ -869,7 +875,7 @@ class UploadTask(BaseCodecovTask, name=upload_task_name):
             result = upload_processor_task.apply_async(kwargs=processor_task_kwargs)
             if result and result.id:
                 scheduled_task_ids.append(result.id)
-        return tuple(scheduled_task_ids)
+        return ScheduledTasksResult(scheduled_task_ids)
 
     def _schedule_bundle_analysis_processing_task(
         self,
