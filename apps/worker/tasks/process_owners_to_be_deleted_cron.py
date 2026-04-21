d4d4f523063deee109855f05db3317fa7bdd7d8f
jasonford <jason.ford@sentry.io>
2026-04-21 13:32:10 -0500

    update celery

--- apps/worker/tasks/process_owners_to_be_deleted_cron.py
+++ apps/worker/tasks/process_owners_to_be_deleted_cron.py
@@ -30,7 +30,7 @@ class ProcessOwnersToBeDeletedCronTask(
 
     @classmethod
     def get_min_seconds_interval_between_executions(cls) -> int:
-        return 60 * 10  # 10 minutes — must be less than the 15-min crontab interval
+        return 60 * 45  # this is  crontab interval for jobs that have large data sets that timeout. 
 
     def run_cron_task(self, db_session, *args, **kwargs) -> dict[str, Any]:
         """
